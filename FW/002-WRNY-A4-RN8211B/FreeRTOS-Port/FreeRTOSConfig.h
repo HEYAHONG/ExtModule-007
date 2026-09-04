e00080068b12d9814232c43c6d7cb7cc56c5b13e
HEYAHONG <2229388563@qq.com>
2026-09-04 11:38:51 +0800

    [002-WRNY-A4-RN8211B] 关闭软件定时器以节省空间

--- FW/002-WRNY-A4-RN8211B/FreeRTOS-Port/FreeRTOSConfig.h
+++ FW/002-WRNY-A4-RN8211B/FreeRTOS-Port/FreeRTOSConfig.h
@@ -226,7 +226,7 @@ extern uint32_t SystemCoreClock;
  * FreeRTOS/source/timers.c source file must be included in the build if
  * configUSE_TIMERS is set to 1.  Default to 0 if left undefined.  See
  * https://www.freertos.org/RTOS-software-timer.html. */
-#define configUSE_TIMERS                1
+#define configUSE_TIMERS                0
 
 /* configTIMER_TASK_PRIORITY sets the priority used by the timer task.  Only
  * used if configUSE_TIMERS is set to 1.  The timer task is a standard FreeRTOS
@@ -666,7 +666,7 @@ extern uint32_t SystemCoreClock;
 #define INCLUDE_uxTaskGetStackHighWaterMark    1
 #define INCLUDE_xTaskGetIdleTaskHandle         1
 #define INCLUDE_eTaskGetState                  1
-#define INCLUDE_xTimerPendFunctionCall         1
+#define INCLUDE_xTimerPendFunctionCall         0
 #define INCLUDE_xTaskAbortDelay                1
 #define INCLUDE_xTaskGetHandle                 1
 #define INCLUDE_xTaskResumeFromISR             1
